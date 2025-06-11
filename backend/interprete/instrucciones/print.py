cd502cb26fdcc3d70650e8b53fafcd7fe7eab044
Byron Galicia <3007571230101@ingenieria.usac.edu.gt>
2025-06-11 09:54:20 -0600

    Objeto instruccion y print de objeto

---
+++ backend/interprete/instrucciones/print.py
@@ -0,0 +1,29 @@
+from interprete.otros.ast import *
+from interprete.otros.tipos import TipoDato
+from .instruccion import Instruccion
+from interprete.otros.enviroment import Enviroment
+from interprete.otros.consola import Consola
+
+class Print(Instruccion):
+    def __init__(self, text_val:str, argumento, linea, columna):
+        super().__init__(text_val, linea, columna)
+        self.text_val = text_val
+        self.argumento = argumento
+    
+    def ejecutar(self, env:Enviroment):
+        exp = self.argumento.ejecutar(env)
+
+        # Validar que no haya un error en la expresion
+        if exp.tipo == TipoDato.ERROR:
+            print("Semántico", f'Error en la expresión de la funcion print()', self.linea, self.columna)
+            return self
+        
+        print(f'Consola: {exp.valor}')
+        # Consola.addConsola(exp.valor)
+    
+    def recorrerArbol(self, raiz:Nodo):
+        id = AST.generarId()
+        hijo = Nodo(id=id, valor='PRINT', hijos=[])
+        raiz.addHijo(hijo)                                    
+        self.argumento.recorrerArbol(hijo)
+    
