73c2e8f34c0d71cd0135bf5ce2a806054fce3470
AlvaroCeballos <a.ceballos.ivddb@gmail.com>
2025-06-10 22:46:44 -0600

    Se crea clase para la tabla de errores lexico, sintactico y semantico

--- backend/interprete/otros/errores.py
+++ backend/interprete/otros/errores.py
@@ -13,3 +13,30 @@ class Error:
             'columna': self.columna,
             'descripcion': self.descripcion
         }
+    
+#Se crea la clase de TablaErrores para manejar una lista de errores
+class TablaErrores:
+    errores:Error = []   # Lista de errores
+    
+    def __init__(self):
+        pass
+
+    @classmethod
+    def addError(cls, error:Error):
+        cls.errores.append(error)
+    
+    @classmethod
+    def getTablaErrores(cls):
+        return cls.errores
+    
+    @classmethod
+    def serializarTBErrores(cls):
+        errores = []
+        for error in cls.errores:
+            errores.append(error.serializar())
+        return errores
+    
+    @classmethod
+    def cleanTablaErrores(cls):
+        cls.errores = []
+    
