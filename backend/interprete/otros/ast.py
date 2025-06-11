1334bd21fa78876252ef9284b6121796b404574a
AlvaroCeballos <a.ceballos.ivddb@gmail.com>
2025-06-11 08:21:11 -0600

    Se crea clase Nodo y AST junto con funciones para graficar a futuro

--- backend/interprete/otros/ast.py
+++ backend/interprete/otros/ast.py
@@ -23,3 +23,45 @@ class Nodo:
     
     def getHijos(self):
         return self.hijos
+    
+
+class AST:
+    id:int = 0
+    def __init__(self, instrucciones):
+        self.instrucciones = instrucciones
+    
+    def getAST(self):
+        declaraciones = ''
+        conexiones = ''
+        raiz = Nodo(id=0, valor='INSTRUCCIONES', hijos=[])
+
+        try:
+            for instruccion in self.instrucciones:
+                instruccion.recorrerArbol(raiz)
+        except TypeError as e:
+                return
+        declaraciones = f'\t{raiz.getId()} [label = "{raiz.getValor()}"];\n'
+        declaraciones, conexiones = self.graficarArbol(raiz, declaraciones, conexiones)
+        dot = 'digraph {\n' + declaraciones + conexiones + '}\n'
+        
+        filename = "backend/AST.dot"
+
+        archivo = open(filename, "w")
+        archivo.write(dot)
+        archivo.close()
+        
+        check_call(['dot','-Tpng',filename,'-o', "backend/AST.png"])
+        Consola.addConsola('AST generado con éxito.')
+        return {'dot': dot}
+
+    def graficarArbol(self, raiz:Nodo, declaraciones:str, conexiones:str):
+        for hijo in raiz.getHijos():
+            declaraciones += f'\t{hijo.getId()} [label = "{hijo.getValor()}"];\n'
+            conexiones += f'\t{raiz.getId()} -> {hijo.getId()};\n'
+            declaraciones, conexiones = self.graficarArbol(hijo, declaraciones, conexiones)
+        return declaraciones, conexiones
+
+    @classmethod
+    def generarId(cls):
+        cls.id += 1
+        return cls.id
